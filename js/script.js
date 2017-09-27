516ea27c237eb86901b901f69eedf629a0ff2dcd
Marta P <mapa.startuj@gmail.com>
2017-09-27 02:50:50 +0200

    new test case

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
+var info = getInfo(rValue);
+var rValue = calculate(a, b);
 function fun(a, b) {
-	rValue = calculate(a, b);
 
 	document.getElementById('result').value = rValue;
 
+	getInfo(rValue);
+	
+	alert(info);
+	console.log('The result with variable a =' + a + ' and b = ' + b + ' is ' + rValue + ', ' + info);
+}
+
+function calculate(a, b) {
+	return (a * a) + (2 * a * b) - (b * b);
+}
+
+function getInfo(rValue) {
 	if (rValue > 0) {
 		info = 'The result is positive';
 	} else if (rValue < 0) {
@@ -13,10 +25,5 @@ function fun(a, b) {
 		info = 'Incorrect variable values - type again';
 	}
 
-	alert(info);
-	console.log('The result with variable a =' + a + ' and b = ' + b + ' is ' + rValue + ', ' + info);
-}
-
-function calculate(a, b) {
-	return (a * a) - (2 * a * b) + (b * b)
+	return info;
 }
